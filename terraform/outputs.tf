eeec455bf9637ef1bd92a3aa60318256dca85e28
Lokesh Repaka <repaka@pdx.edu>
2026-09-04 11:51:55 -0700

    Add CloudFront distribution and secure S3 frontend access

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,13 @@ output "frontend_bucket_arn" {
   description = "ARN of the frontend S3 bucket"
   value       = aws_s3_bucket.frontend.arn
 }
+
+output "cloudfront_distribution_id" {
+  description = "ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.frontend.id
+}
+
+output "cloudfront_domain_name" {
+  description = "CloudFront domain name for the frontend"
+  value       = aws_cloudfront_distribution.frontend.domain_name
+}
